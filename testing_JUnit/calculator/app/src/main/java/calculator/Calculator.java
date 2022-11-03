a62f4211384e98ab42201c2ce2a80d85e070c938
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2022-11-03 11:27:03 +0000

    Exercise - Writing the first test with JUnit and debugging to make the test pass.

---
+++ testing_JUnit/calculator/app/src/main/java/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+class Calculator{
+  public static void main(String[] args) {
+
+  }
+ 
+  Integer add(int integer1, int integer2) {
+    return integer1 + integer2;
+  }
+ 
+
+}
