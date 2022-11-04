92589f261cb000d56f0e958c5b32c0fe67d61a6b
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2022-11-04 23:36:09 +0000

    The initial project setup was completed using Gradle. Next, I created my first test for the Todo class and initialised it with an empty list of tasks.

---
+++ testing_JUnit/todo/app/src/test/java/todo/TodoTest.java
@@ -0,0 +1,17 @@
+package todo;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class TodoTest {
+  @Test 
+  public void shouldStartWithEmptyList() {
+    Todo todo = new Todo();
+    assertTrue("Initial task list is empty", todo.tasks.isEmpty()); 
+
+  }
+ 
+}
